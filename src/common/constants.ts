2245a855653ee8c309fcbca2eaa494c19aa1b014
Hadas Meitav <hadas@Hadass-MacBook-Pro.local>
2021-07-05 11:54:07 +0300

    wip

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,5 +1,5 @@
 export const validPhoneNumbersExamples =
-  'Examples for a valid phone number: +41 311111111, +41 (0)31 633 60 01, +49 9072 1111, etc..';
+  'examples for a valid phone number: +41 311111111, +41 (0)31 633 60 01, +49 9072 1111, etc..';
 
 export const Errors = {
   user: {
@@ -17,8 +17,8 @@ export const Errors = {
       reasons: {
         uniquePhoneNumber: 'phone number already exists',
         phoneNumberValidation:
-          `phone number must be a valid phone number.` +
-          `please make sure you've added the country code with (+) in the beginning.` +
+          `phone number must be a valid phone number. ` +
+          `please make sure you've added the country code with (+) in the beginning. ` +
           `${validPhoneNumbersExamples}`,
       },
     },
