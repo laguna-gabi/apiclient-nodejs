ec83a73bbcee38f9adb92bbd8f332e63e8375ea2
hadasmeitav <hadas@lagunahealth.com>
2021-08-24 13:16:56 +0300

    fix: notification live tests add retry (#101)

--- test/live/notifications.live.spec.ts
+++ test/live/notifications.live.spec.ts
@@ -6,28 +6,48 @@ import * as faker from 'faker';
 import { delay } from '../common';
 
 describe('live: notifications (one signal)', () => {
-  it('should register a device on voip onesignal project and send video notification', async () => {
+  let notificationsService: NotificationsService;
+  const delayTime = 3000;
+  const RETRY_MAX = 3;
+
+  beforeAll(() => {
     const configService = new ConfigsService();
     const httpService = new HttpService();
-    const notificationsService = new NotificationsService(configService, httpService);
+    notificationsService = new NotificationsService(configService, httpService);
+  });
 
-    const params = { token: 'sampleiospushkittoken', externalUserId: v4() };
-    const result = await notificationsService.register(params);
-    expect(result).toBeTruthy();
+  it(
+    'should register a device on voip onesignal project and send video notification',
+    async () => {
+      const params = { token: 'sampleiospushkittoken', externalUserId: v4() };
+      const result = await notificationsService.register(params);
+      expect(result).toBeTruthy();
 
-    /**
-     * one signal send notification causing an error if sent immediatly after registering a device.
-     * So, we'll delay the send for 5 seconds(less than that sometimes doesn't work)
-     * result.data.errors: ["All included players are not subscribed"]
-     */
-    await delay(5000);
+      /**
+       * If sent immediately after registering a device, calling send notification causes an error.
+       * We'll retry and delay sending a notification for a few seconds.
+       * result.data.errors: ["All included players are not subscribed"]
+       */
+      let current = 0;
+      while (current < RETRY_MAX) {
+        await delay(delayTime);
 
-    const sendResult = await notificationsService.send({
-      externalUserId: params.externalUserId,
-      mobilePlatform: MobilePlatform.ios,
-      notificationType: NotificationType.voip,
-      payload: { contents: { en: faker.lorem.sentence() }, heading: { en: faker.lorem.word() } },
-    });
-    expect(sendResult).toBeTruthy();
-  }, 10000);
+        const result = await notificationsService.send({
+          externalUserId: params.externalUserId,
+          mobilePlatform: MobilePlatform.ios,
+          notificationType: NotificationType.voip,
+          payload: {
+            contents: { en: faker.lorem.sentence() },
+            heading: { en: faker.lorem.word() },
+          },
+        });
+
+        current = result ? RETRY_MAX : current + 1;
+        if (current === RETRY_MAX || result) {
+          expect(result).toBeTruthy();
+        }
+      }
+    },
+    delayTime * RETRY_MAX + 5000,
+  );
 });
