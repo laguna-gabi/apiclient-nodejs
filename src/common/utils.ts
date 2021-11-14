35d61816e9dacdf38d34211b0b5bd14932cda738
Hadas Meitav <hadasmeitav@gmail.com>
2021-11-14 16:08:20 +0200

    wip

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,5 +1,5 @@
-export const filterNonNullFields = (params) => {
+export function filterNonNullFields<T>(params): T | Record<string, never> {
   return Object.keys(params)
     .filter((k) => params[k] !== null)
     .reduce((a, k) => ({ ...a, [k]: params[k] }), {});
-};
+}
