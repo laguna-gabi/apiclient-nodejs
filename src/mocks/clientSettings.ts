ab493d27b1a04cefd62b023db48412e4f6451eb4
hadasmeitav <hadas@lagunahealth.com>
2021-12-06 16:32:45 +0200

    refactor: mocks (#35)

--- src/mocks/clientSettings.ts
+++ src/mocks/clientSettings.ts
@@ -5,14 +5,15 @@ import {
   Language,
   Platform,
   generatePhone,
+  generateZipCode,
 } from '../index';
 import { v4 } from 'uuid';
-import { name } from 'faker';
+import { internet, name } from 'faker';
 
 export type UpdateMemberSettingsType = Omit<IUpdateClientSettings, 'avatar'>;
-export type UpdateUserSettingsType = Omit<
+export type UpdateUserSettingsType = Pick<
   IUpdateClientSettings,
-  'id' | 'phone' | 'firstName' | 'lastName' | 'avatar'
+  'id' | 'type' | 'phone' | 'firstName' | 'lastName' | 'avatar'
 >;
 
 export class ObjectUpdateMemberSettingsClass {
@@ -23,23 +24,63 @@ export class ObjectUpdateUserSettingsClass {
   constructor(readonly objectUpdateMemberSettings: UpdateMemberSettingsType) {}
 }
 
-export const generateUpdateMemberSettingsMock = (): UpdateMemberSettingsType => {
+export const generateUpdateMemberSettingsMock = ({
+  id = v4(),
+  phone = generatePhone(),
+  firstName = name.firstName(),
+  lastName = name.lastName(),
+  orgName = name.firstName(),
+  honorific = Honorific.reverend,
+  zipCode = generateZipCode(),
+  language = Language.en,
+}: {
+  id?: string;
+  phone?: string;
+  firstName?: string;
+  lastName?: string;
+  orgName?: string;
+  honorific?: Honorific;
+  zipCode?: string;
+  language?: Language;
+} = {}): UpdateMemberSettingsType => {
   return {
     type: InnerQueueTypes.updateClientSettings,
-    id: v4(),
-    phone: generatePhone(),
-    firstName: name.firstName(),
-    lastName: name.lastName(),
-    //only member
-    orgName: name.firstName(),
+    id,
+    phone,
+    firstName,
+    lastName,
+    orgName,
+    honorific,
+    zipCode,
+    language,
     platform: Platform.web,
-    isPushNotificationsEnabled: false,
+    isPushNotificationsEnabled: true,
     isAppointmentsReminderEnabled: true,
     isRecommendationsEnabled: true,
     externalUserId: v4(),
     firstLoggedInAt: new Date(),
-    honorific: Honorific.reverend,
-    zipCode: '91210',
-    language: Language.en,
+  };
+};
+
+export const generateUpdateUserSettingsMock = ({
+  id = v4(),
+  phone = generatePhone(),
+  firstName = name.firstName(),
+  lastName = name.lastName(),
+  avatar = internet.avatar(),
+}: {
+  id?: string;
+  phone?: string;
+  firstName?: string;
+  lastName?: string;
+  avatar?: string;
+} = {}): UpdateUserSettingsType => {
+  return {
+    type: InnerQueueTypes.updateClientSettings,
+    id,
+    phone,
+    firstName,
+    lastName,
+    avatar,
   };
 };
--- src/mocks/generators.ts
+++ src/mocks/generators.ts
@@ -1,3 +1,6 @@
+import { address } from 'faker';
+import { lookup } from 'zipcode-to-timezone';
+
 export const generatePhone = () => {
   const random = () => Math.floor(Math.random() * 9) + 1;
 
@@ -7,4 +10,19 @@ export const generatePhone = () => {
   }
 
   return phone;
+};
+
+export const generateZipCode = (): string => {
+  while (true) {
+    const zipCode = address.zipCode('#####');
+    /**
+     * On occasions, faker generates invalid zipcodes. we'll try to generate
+     * timezone, if it worked, we'll return the zipCode and exit the loop
+     * Usually this works in the 1st time, so rarely we'll do it twice.
+     */
+    const timeZone = lookup(zipCode);
+    if (timeZone) {
+      return zipCode;
+    }
+  }
 };
