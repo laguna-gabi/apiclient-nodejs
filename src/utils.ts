65ec89074d025082039591f6c49123edb1ec3fb7
laguna-gabi <88610733+laguna-gabi@users.noreply.github.com>
2022-01-02 16:07:39 +0200

    remove logs from iris in test [sc-3465] (#94)

--- src/utils.ts
+++ src/utils.ts
@@ -8,11 +8,18 @@ export const generateDispatchId = (contentKey: ContentKey, ...params: string[]) 
   return params.length === 0 ? contentKey : `${contentKey}_${params.sort().join('_')}`;
 };
 
-
 /*******************************************************************************
  *********************************** Logger ***********************************
  ******************************************************************************/
 
 export const formatEx = (ex): FailureReason => {
   return { message: ex.message, code: ex.code, stack: ex.stack };
+};
+
+export const mockLogger = (logger) => {
+  jest.spyOn(logger, 'log').mockImplementation(() => undefined);
+  jest.spyOn(logger, 'debug').mockImplementation(() => undefined);
+  jest.spyOn(logger, 'error').mockImplementation(() => undefined);
+  jest.spyOn(logger, 'info').mockImplementation(() => undefined);
+  jest.spyOn(logger, 'warn').mockImplementation(() => undefined);
 };
