73c0c0f90546a9e2abda1d780e4d58cd60554981
nitsanbs <91946556+nitsanbs@users.noreply.github.com>
2022-01-03 09:23:07 +0200

    changed invalid objectId to warning instead of error (#666)

--- src/common/errors.ts
+++ src/common/errors.ts
@@ -212,4 +212,9 @@ export const DbErrors = {
   duplicateKey: 11000,
 };
 
-export const LogAsWarning = new Set([...Errors.values(), 'Forbidden resource', 'Unauthorized']);
+export const LogAsWarning = new Set([
+  ...Errors.values(),
+  'Forbidden resource',
+  'Unauthorized',
+  'Argument passed in must be a single String of 12 bytes or a string of 24 hex characters',
+]);
