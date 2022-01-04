6c84f8f3398e646d46a4d6cfd6788cb5a8d111bf
hadasmeitav <hadas@lagunahealth.com>
2022-01-04 14:51:47 +0200

    chore: set utc time on local process (#106)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { general } from 'config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  process.env.TZ = general.timezone;
+
   await app.listen(3001);
 }
 bootstrap();
