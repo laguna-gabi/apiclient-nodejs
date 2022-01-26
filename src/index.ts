0a73a3f4b0f6cdbaebb12950e9fbb6ec446954ca
hadasmeitav <hadas@lagunahealth.com>
2022-01-26 14:42:02 +0200

    export translation (#134)

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@ export * from './utils';
 export * from './baseLogger';
 export * from './providers';
 export * from './mocks';
+export { translation } from './languages/en.json';
+export { translation as translationEs } from './languages/es.json';
