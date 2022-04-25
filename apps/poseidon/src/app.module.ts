d40c03a9207554e75d95b0db561e0184a379e1bd
laguna-gabi <88610733+laguna-gabi@users.noreply.github.com>
2022-04-25 11:59:51 +0300

    add health controller to poseidon [sc-4918] (#151)

--- apps/poseidon/src/app.module.ts
+++ apps/poseidon/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { TerminusModule } from '@nestjs/terminus';
 import { DbModule } from './db';
+import { HealthController } from './health/health.controller';
 import { ProvidersModule } from './providers';
 
 @Module({
-  imports: [ProvidersModule, DbModule, EventEmitterModule.forRoot()],
-  controllers: [],
+  imports: [ProvidersModule, DbModule, EventEmitterModule.forRoot(), TerminusModule],
+  controllers: [HealthController],
 })
 export class AppModule {}
